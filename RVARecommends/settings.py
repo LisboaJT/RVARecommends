d9c2372746cb0b07d87154e886c5831134eb056b
John Taylor <jptaylor1978@gmail.com>
2024-03-18 16:33:43 +0000

    First rev! Let's goooo!

--- RVARecommends/settings.py
+++ RVARecommends/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uzdwy07e291c+!$s6xoo#6=#p8^&g6ebcdvmubhr_eb2gcgj&)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
